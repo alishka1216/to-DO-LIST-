2fc1e4c679947175a9b7796d0373e017c2ae8232
ALisher <alishka1216@gmail.com>
2021-02-26 19:16:17 +0600

    добавил name и изменил путь

--- source/main/urls.py
+++ source/main/urls.py
@@ -5,7 +5,7 @@ from webapp.views import index_view, article_view, article_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view),  # URL для отображения списка статей
-    path('articles/add/', article_create_view),  # URL для создания статьи
-    path('article/', article_view)  # URL для просмотра деталей статьи
+    path('', index_view, name='article-list'),  # URL для отображения списка статей
+    path('articles/add/', article_create_view, name='article-add'),  # URL для создания статьи
+    path('article/<int:pk>/', article_view, name='article-view')  # URL для просмотра деталей статьи
 ]
