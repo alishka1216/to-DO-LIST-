ad98e7e5a8a91b57fa9920f47cfc5b4cd3528d61
ALisher <alishka1216@gmail.com>
2021-03-01 16:55:11 +0600

    добавил ошибки NONE

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -11,8 +11,7 @@ def index_view(request):
     Представление для отображения списка статей
     """
     articles = Article.objects.all()  # Получаем список статей из базы данных
-    return render(request, 'index.html', context={
-        'articles': articles})  # Возвращаем "скомпилированный" шаблон с использованием переданного списка статей
+    return render(request, 'index.html', context={'articles': articles})  # Возвращаем "скомпилированный" шаблон с использованием переданного списка статей
 
 
 def article_view(request, pk):
@@ -31,13 +30,22 @@ def article_create_view(request):
         return render(request, 'article_create.html', context={'choices': STATUS_CHOICES})
     elif request.method == "POST":  # Если метод запроса POST - будет отображён шаблон просмотра деталей статьи
         title = request.POST.get("title")
+        detailed_description = request.POST.get("detail_discrb")
         status = request.POST.get("status")
         date = request.POST.get("date")
+        if not date:
+            date=None
+        if not detailed_description:
+            detailed_description=None
+        if not title:
+            title=None
 
         article = Article.objects.create(
             title=title,
             status=status,
+            detailed_description=detailed_description,
             date=date
+
         )
 
         return redirect('article-view', pk=article.id)
