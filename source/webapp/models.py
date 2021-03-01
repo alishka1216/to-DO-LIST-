c1104e59fa1180e007e0bb419e7377bdd3724a03
ALisher <alishka1216@gmail.com>
2021-03-01 16:57:20 +0600

    подредактировал статусы

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,13 +5,12 @@ from django.db import models
 STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 class Article(models.Model):
     title = models.CharField(max_length=3000, null=False, blank=False)
+    detailed_description = models.CharField(max_length=3000, null=True, blank=True)
     status = models.CharField(max_length=200, null=True, blank=True, choices=STATUS_CHOICES, default='new')
     date = models.DateField(null=True, blank=True)
 
     class Meta:
         db_table = 'articles'
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
 
-    # def __str__(self):
-    #     return f'{self.id}. {self.author}: {self.title}'
