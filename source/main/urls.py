2960e3dc3f85f6e2bf4c706aa6966209887e42a1
ALisher <alishka1216@gmail.com>
2021-03-03 17:10:39 +0600

    добавил ссылку редактирования

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view, article_view, article_create_view
+from webapp.views import index_view, article_view, article_create_view, article_update_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='article-list'),  # URL для отображения списка статей
     path('articles/add/', article_create_view, name='article-add'),  # URL для создания статьи
-    path('article/<int:pk>/', article_view, name='article-view')  # URL для просмотра деталей статьи
+    path('article/<int:pk>/', article_view, name='article-view'),  # URL для просмотра деталей статьи
+    path('article/update/<int:pk>/', article_update_view, name='article_update')
 ]
